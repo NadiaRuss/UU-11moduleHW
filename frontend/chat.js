38e8ef4dfe25836d94ac550c9c8294248d8fa389
Надежда Яковлева <i160220@mail.ru>
2024-10-07 13:50:22 +0700

    Доработаны некоторые нюансы, файлы js объединены в один

--- frontend/chat.js
+++ frontend/chat.js
@@ -3,10 +3,33 @@ const messageForm = document.getElementById("messageForm");
 const messageInput = document.getElementById("messageInput");
 const socket = new WebSocket("ws://localhost:8080");
 
+let popupClose = document.querySelector(".popup-close");
+let popupOwerflow = document.querySelector(".popup-overflow");
+let loginInput = document.getElementById("loginInput");
+let loginForm = document.getElementById("loginForm");
+
 socket.onopen = (e) => {
   console.log("Соединение прошло успешно!");
 };
+//Проверяем есть ли в хранилище userName и если есть сразу закрываем попап
 let userName = localStorage.getItem("userName");
+if (userName) {
+  popupOwerflow.style.display = "none";
+}
+//При закрытии на крестик UserName не вводится, будет "Незнакомец"
+popupClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    popupOwerflow.style.display = "none";
+  });
+  // При вводе имени пользователя userName сохраняется в localStorage
+  loginForm.onsubmit = (e) => {
+    // e.preventDefault(); нужна перезагрузка страницы
+    if (loginInput.value) {
+      let userName = loginInput.value;
+      localStorage.setItem("userName", userName);
+      popupOwerflow.style.display = "none";
+    }
+  };
 
 socket.onmessage = (event) => {
   const message = JSON.parse(event.data);
@@ -14,13 +37,12 @@ socket.onmessage = (event) => {
   if (message.type === "system") {
     messageElm.classList.add("system-message");
   }
-
   if (userName) {
-    popupOwerflow.style.display = "none";
     messageElm.textContent = `${userName} : ${message.content}`;
   } else {
     messageElm.textContent = `Незнакомец : ${message.content}`;
   }
+
   chat.appendChild(messageElm);
   chat.scrollTop = chat.scrollHeight;
 };
